7d0a31b7bb992e9043a099db9c4ddf56f49cfec9
Momo0924N <115760638+Momo0924N@users.noreply.github.com>
2022-10-15 11:26:12 +0900

    Create TrangibleAsset.java

---
+++ TrangibleAsset.java
@@ -0,0 +1,14 @@
+piblic abstruct class TrangibleAsset{
+  private String nama;
+  private int price;
+  private String color;
+  
+  public TrangibleAsset(STring name, int price, String color){
+  this.name = name;
+  this.price = price;
+  this.color = color;  
+  }
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
+}
